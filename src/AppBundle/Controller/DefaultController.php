2627fbc921c5cb057e9f16a0b7c7a7f86f3c09d3
Koshkin Anton <nikshok@bk.ru>
2017-03-14 22:35:18 +0700

    first stage - user, sender

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,75 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+
+/**
+ * @Route("/vk/controller", name="homepage")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @param Request $request
+     * @return mixed|\Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/info")
      */
-    public function indexAction(Request $request)
+    public function infoAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        $json_string = '{"id":210700286,"first_name":"Lindsey","last_name":"Stirling"}';
+
+        $request_user = json_decode($json_string, true);
+
+        $user = new User();
+
+        $user->setFirstName($request_user['first_name']);
+        $user->setLastName($request_user['last_name']);
+        $user->setImportId($request_user['id']);
+        $user->setProviderId(User::PROVIDER_VK);
+
+        $users = $this->getDoctrine()->getRepository(User::class)->findOneBy([
+            'importId' => $user->getImportId(), 'providerId' => $user->getProviderId()
         ]);
+
+        if (isset($users)) {
+
+            return $this->render('@App/default/base.html.twig', [
+                'user' => $user,
+            ]);
+
+        } else {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->render('@App/default/base.html.twig', [
+                'user' => $user,
+            ]);
+        }
+
+    }
+
+    /**
+     * @Route("/sendMessage")
+     * @param Request $request
+     * @return Response
+     */
+    public function sendMessage(Request $request)
+    {
+        $sender = $this->get('sender_service');
+        $sender->sendMessage(87305277, 'hello');
+
+        $response = new Response();
+
+        $response->setStatusCode(Response::HTTP_OK);
+
+        return $response;
+
     }
 }
---
+++ src/AppBundle/Services/MessageSenders/TestSender.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Services\MessageSenders;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class TestSender
+{
+    private $peer_id;
+    private $access_token;
+    private $v;
+
+    public function __construct($peer_id, $access_token, $v)
+    {
+        $this->peer_id = $peer_id;
+        $this->access_token = $access_token;
+        $this->v = $v;
+    }
+
+    public function sendMessage($user_id, $message)
+    {
+        $parameters = [
+            'user_id' => $user_id,
+            'random_id' => mt_rand(1, 99999),
+            'peer_id' => $this->peer_id,
+            'message' => $message,
+            'access_token' => $this->access_token,
+            'v' => $this->v,
+        ];
+
+        $url = 'https://api.vk.com/method/messages.send?' . http_build_query($parameters);
+
+        $curl = curl_init('');
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_REFERER, 'http://google.com');
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0');
+
+        $data = curl_exec($curl);
+
+        curl_close($curl);
+
+        $request = new Request();
+
+    }
+}
