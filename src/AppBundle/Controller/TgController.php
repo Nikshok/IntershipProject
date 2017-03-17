f889feee25b0aec706e61002b55f2b2b72841b19
Koshkin Anton <nikshok@bk.ru>
2017-03-17 19:45:21 +0700

    add img

---
+++ src/AppBundle/Controller/TgController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/**
+ * @Route("/tg")
+ */
+class TgController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return mixed|\Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/")
+     */
+    public function indexAction(Request $request)
+    {
+        $json_string = '{"id":87305277,"first_name":"Lindsey","last_name":"Stirling","message":"фываыфва"}';
+
+        $request_user = json_decode($json_string, true);
+
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+        $user = $userRepository->findOneByImportIdAndProviderId($request_user['id'], User::PROVIDER_VK);
+
+        if (!($user)) {
+
+            $user = new User();
+
+            $user->setFirstName($request_user['first_name']);
+            $user->setLastName($request_user['last_name']);
+            $user->setImportId($request_user['id']);
+            $user->setProviderId(User::PROVIDER_VK);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+        }
+
+        $parser = $this->get('message_parser_service');
+
+        $eventArr = $parser->parseMessage($request_user['message']);    //return ['event_name', 'param']
+
+        //How to use game_event
+        //$gameEvent = $this->get($eventArr['event_name']);
+
+        //How to use message_driver in GameListners, don't forget include message_driver in GameListner
+        $sender = $this->get('message_driver_service');
+        $sender->addMessage($user, 'Smth message ($message)');
+        $sender->addMessage($user, 'Smth message ($message) 2');
+        $sender->addMessage($user, 'Smth message ($message) 3');
+
+        $sender->execute();
+
+        return new Response(Response::HTTP_OK);
+    }
+
+}
--- src/AppBundle/Services/MessageSenders/MessageSenderVK.php
+++ src/AppBundle/Services/MessageSenders/MessageSenderVK.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Services\MessageSenders;
 
 use AppBundle\Entity\User;
+use GuzzleHttp\Client;
 use Symfony\Component\HttpFoundation\Request;
 
 class MessageSenderVK
@@ -36,5 +37,21 @@ class MessageSenderVK
         curl_exec($curl);
 
         curl_close($curl);
+
+        $url = "https://pu.vk.com/c638125/upload.php?act=do_add&mid=87305277&aid=-64&gid=142517072&hash=7614cf4c65951d3ea70eedf5ca8d951d&rhash=be24453aa143a7ba33951463f5e90b41&swfupload=1&api=1&mailphoto=1";
+
+        $client = new Client();
+
+        $response = $client->request('POST', $url, [
+            'multipart' => [
+                [
+                    'name' => 'photo',
+                    'contents' => fopen('/home/nikshok/Pictures/test.jpg', 'r'),
+                ],
+            ]
+        ]);
+
+        print_r($response);
+
     }
 }
