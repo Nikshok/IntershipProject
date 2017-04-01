0bf99c7b7ff9a5916dde50ba0f1a71b4b5b47f48
Noveo Developer 8 <noveo-dev8@noveogroup.com>
2017-04-02 00:57:52 +0700

    add test case

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Game;
 use AppBundle\Entity\User;
+use AppBundle\Entity\GameQuestion;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,12 +24,19 @@ class TestController extends Controller
      */
     public function gameAction(Request $request)
     {
-        foreach ($this->getDoctrine()->getRepository(Game::class)->findAll() as $game) {
+        $em = $this->getDoctrine()->getManager();
 
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($game);
-            $em->flush();
+        foreach ($this->getDoctrine()->getRepository(GameQuestion::class)->findAll() as $row) {
+            $em->remove($row);
         }
+
+        $em->flush();
+
+        foreach ($this->getDoctrine()->getRepository(Game::class)->findAll() as $game) {
+            $em->remove($game);
+        }
+
+        $em->flush();
 
         $userRepository = $this->getDoctrine()->getRepository(User::class);
 
@@ -41,6 +49,9 @@ class TestController extends Controller
         $sender = $this->get('message_driver_service');
 
         //в холостую
+        $event = new \AppBundle\Game\GameAnswerEvent($this->getDoctrine(), $sender);
+        $event->fire($user1,1);
+
         $event = new \AppBundle\Game\GameCapitulateEvent($this->getDoctrine(), $sender);
         $event->fire($user1);
 
@@ -141,5 +152,77 @@ class TestController extends Controller
 
     }
 
+    /**
+     * @param Request $request
+     * @return mixed|\Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/game2")
+     */
+    public function game2Action(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
 
+        foreach ($this->getDoctrine()->getRepository(GameQuestion::class)->findAll() as $row) {
+            $em->remove($row);
+        }
+
+        $em->flush();
+
+        foreach ($this->getDoctrine()->getRepository(Game::class)->findAll() as $game) {
+            $em->remove($game);
+        }
+
+        $em->flush();
+
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+
+        //select fake user 1
+        $user1 = $userRepository->findOneById(1);
+
+        //select fake user 2
+        $user2 = $userRepository->findOneById(2);
+
+        $sender = $this->get('message_driver_service');
+
+        //первый ищет игру
+        $event = new \AppBundle\Game\GameSearchEvent($this->getDoctrine(), $sender);
+        $event->fire($user1);
+
+        //второй ищет игру
+        $event = new \AppBundle\Game\GameSearchEvent($this->getDoctrine(), $sender);
+        $event->fire($user2);
+
+        //первый соглашается
+        $event = new \AppBundle\Game\GameStartEvent($this->getDoctrine(), $sender);
+        $event->fire($user1);
+
+        $sender->execute();
+
+        //второй отвечает один раз
+        $event = new \AppBundle\Game\GameAnswerEvent($this->getDoctrine(), $sender);
+        $event->fire($user2,1);
+
+        $sender->execute();
+
+
+        //первый отвечает на все вопросы
+        for ($i=0; $i<25; $i++) {
+
+            $event = new \AppBundle\Game\GameAnswerEvent($this->getDoctrine(), $sender);
+            $event->fire($user1,1);
+            $sender->execute();
+        }
+
+        //второй отвечает на все вопросы
+        for ($i=0; $i<25; $i++) {
+
+            $event = new \AppBundle\Game\GameAnswerEvent($this->getDoctrine(), $sender);
+            $event->fire($user2,1);
+            $sender->execute();
+        }
+
+
+        return new Response('__');
+
+    }
 }
