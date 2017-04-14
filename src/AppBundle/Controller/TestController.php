a7871a239325ba556ae65cbcd91f1e529abb2ba8
Koshkin Anton <nikshok@bk.ru>
2017-04-14 22:51:12 +0700

    asd

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -92,7 +92,6 @@ class TestController extends Controller
         //второй отменяет игру
         $event = new \AppBundle\Game\GameCancelEvent($this->getDoctrine(), $sender);
         $event->fire($user2);
-
         //первый ищет игру
         $event = new \AppBundle\Game\GameSearchEvent($this->getDoctrine(), $sender);
         $event->fire($user1);
@@ -148,7 +147,6 @@ class TestController extends Controller
         $sender->execute();
 
         return new Response('__');
-
 
     }
 
@@ -222,6 +220,109 @@ class TestController extends Controller
         }
 
 
+        return new Response('__');
+
+    }
+
+    /**
+     * @Route("/game3")
+     */
+    public function game3Action(Request $request)
+    {
+        if (!$request = json_decode($request->getContent(), true)) {
+            return new Response('OK');
+        }
+
+        if (!isset($request['object'])) {
+            return new Response('OK');
+        }
+
+        $request_user = $request['object'];
+
+        if (!isset($request_user['user_id']) || !isset($request_user['body'])) {
+            return new Response('OK');
+        }
+
+        if ($request_user['user_id'] == null || $request_user['body'] == null) {
+            return new Response('OK');
+        }
+
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+        $user = $userRepository->findOneByImportIdAndProviderId($request_user['user_id'], User::PROVIDER_VK);
+
+        if (!($user)) {
+
+            $user = new User();
+
+            $vkService = $this->get('vk_service');
+            $userInfo = $vkService->getUserInfo($request_user['user_id']);
+
+            $user->setFirstName($userInfo['first_name']);
+            $user->setLastName($userInfo['last_name']);
+            $user->setAvatar($userInfo['avatar']);
+            $user->setImportId($request_user['user_id']);
+            $user->setProviderId(User::PROVIDER_VK);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
+        $parser = $this->get('message_parser_service');
+
+        $eventArr = $parser->parseMessage($request_user['body']);    //return ['event_name', 'param']
+
+        $sender = $this->get('message_driver_service');
+
+        $eventClassName = '\AppBundle\Game\\' . $eventArr['event_name'];
+        print_r($eventArr['event_name']);
+        return new Response('__');
+
+    }
+
+    /**
+     * @Route("/game4")
+     */
+    public function game4Action(Request $request)
+    {
+        $str = 'ответ 1';
+
+        print_r(mb_strlen(trim($str)));
+
+        if (!$request = json_decode($request->getContent(), true)) {
+            return new Response('OK');
+        }
+
+        if (!isset($request['object'])) {
+            return new Response('OK');
+        }
+
+        $request_user = $request['object'];
+
+        if (!isset($request_user['user_id']) || !isset($request_user['body'])) {
+            return new Response('OK');
+        }
+
+        if ($request_user['user_id'] == null || $request_user['body'] == null) {
+            return new Response('OK');
+        }
+
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+
+        //select fake user 1
+        $user1 = $userRepository->findOneById(1);
+        $parser = $this->get('message_parser_service');
+
+        $eventArr = $parser->parseMessage($request_user['body']);    //return ['event_name', 'param']
+
+        $sender = $this->get('message_driver_service');
+
+        $eventClassName = '\AppBundle\Game\\' . $eventArr['event_name'];
+        $event = new $eventClassName($this->getDoctrine(), $sender);
+        $event->fire($user1, $eventArr['param']);
+//        print_r($eventArr['event_name']);
+
+        $sender->execute();
         return new Response('__');
 
     }
--- src/AppBundle/Game/Listeners/GameTopListener.php
+++ src/AppBundle/Game/Listeners/GameTopListener.php
@@ -1,19 +1,20 @@
 <?php
 
 namespace AppBundle\Game\Listeners;
+
 use AppBundle\Entity\Phrase;
 use AppBundle\Entity\User;
 
 class GameTopListener extends GameAbstractListener
 {
-    public function fire(User $user, array $users) {
+    public function fire(User $user, array $users)
+    {
         $phrase = $this->doctrine->getRepository(Phrase::class)->findOneBy(['categoryId' => 11]);
+
         $message = $phrase->getPhrase()."<br>";
 
         foreach ($users as $key => $value) {
-
                 $message .= ($key+1) . "&#8419; " . $value[0]->getFullName() . " " .  $value[1] . "<br>";
-
         }
 
         $this->messageDriver->addMessage($user, $message);
